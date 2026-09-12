7c9688e27878addeb24e22103b3b80a99facc54e
vineet <vineetbhandari5110@gmail.com>
2026-09-13 00:08:57 +0530

    fix: Lint fixes.

--- chapter2-basics/fizzbuzz.go
+++ chapter2-basics/fizzbuzz.go
@@ -2,33 +2,33 @@ package main
 
 import "fmt"
 
-func fizzbuzz(){
-	for i:=1;i<=100;i++{
-		if i%3==0 && i%5==0{
+func fizzbuzz() {
+	for i := 1; i <= 100; i++ {
+		if i%3 == 0 && i%5 == 0 {
 			fmt.Printf("fizzbuzz")
-		}else if i%3==0{
+		} else if i%3 == 0 {
 			fmt.Printf("fizz")
-		}else if i%5==0{
+		} else if i%5 == 0 {
 			fmt.Printf("buzz")
-		}else{
-			fmt.Printf("%d",i)
+		} else {
+			fmt.Printf("%d", i)
 		}
 		/*
-		switch {
-		case i%3==0 && i%5==0:
-			fmt.Printf("fizzbuzz")
-		case i%3==0:
-			fmt.Printf("fizz")
-		case i%5==0:
-			fmt.Printf("buzz")
-		default:
-			fmt.Printf("%d",i)
-		}
+			switch {
+			case i%3==0 && i%5==0:
+				fmt.Printf("fizzbuzz")
+			case i%3==0:
+				fmt.Printf("fizz")
+			case i%5==0:
+				fmt.Printf("buzz")
+			default:
+				fmt.Printf("%d",i)
+			}
 		*/
 		fmt.Println()
 	}
 }
 
-func main(){
+func main() {
 	fizzbuzz()
 }
--- chapter2-basics/loops.go
+++ chapter2-basics/loops.go
@@ -2,35 +2,35 @@ package main
 
 import "fmt"
 
-func for_loop(){
-	for i := 0;i<10;i++{
+func for_loop() {
+	for i := 0; i < 10; i++ {
 		//fmt.Println(i)
-		fmt.Printf("%d\n",i+1)
+		fmt.Printf("%d\n", i+1)
 	}
 }
 
-func goto_loop(){
-	var i int= 0
-	here:
+func goto_loop() {
+	var i int = 0
+here:
 	i += 1
 	fmt.Println(i)
-	if i<10{
+	if i < 10 {
 		goto here
 	}
 }
 
-func array_loop(){
-	var arr [10] int
-	for i := range(10){
-		arr[i]=i+1
+func array_loop() {
+	var arr [10]int
+	for i := range 10 {
+		arr[i] = i + 1
 	}
 	/*for i := range(10){
 		fmt.Println(arr[i])
 	}*/
-	fmt.Printf("%v\n",arr)
+	fmt.Printf("%v\n", arr)
 }
 
-func main(){
+func main() {
 	// for_loop()
 	// goto_loop()
 	array_loop()
